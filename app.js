51c94b7ddb218c945962020375e88c3c2ba2a593
Henri Collin <henri.collin@student.uclouvain.be>
2022-05-13 10:15:44 +0200

    modifs

--- app.js
+++ app.js
@@ -5,13 +5,11 @@ const path = require('path');
 
 const port = process.env.port || 5000;
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static('build'));
-    console.log("here")
-    app.get('*', (req,res) => {
-        req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-    })
-}
+app.use(express.static('build'));
+console.log("here")
+app.get('*', (req,res) => {
+    req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+})
 
 app.listen(port, (err) => {
     if (err) return console.log(err)
